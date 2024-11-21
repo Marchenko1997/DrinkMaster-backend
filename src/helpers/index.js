e49323672667384799e4223870fc9d645f9dd1f8
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-21 13:16:01 +0100

    creating schemas for User

---
+++ src/helpers/index.js
@@ -0,0 +1,13 @@
+import HttpError from './HttpError.js';
+import ctrlWrapper from './ctrlWrapper.js';
+import handleMongooseError from './handleMongooseError.js';
+import sendEmail from './sendEmail.js';
+import fullYearsCount from './fullYearsCount.js';
+
+export {
+  HttpError,
+  ctrlWrapper,
+  handleMongooseError,
+  sendEmail,
+  fullYearsCount,
+};
--- src/models/users.js
+++ src/models/users.js
@@ -1,3 +1,92 @@
 import { Schema, model } from 'mongoose';
 import Joi from 'joi';
-import { handleMongooseError } from '../helpers/handleMongooseError';
+import { handleMongooseError } from '../helpers/index';
+
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+const defBirthday = new Date();
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for user'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required: example@example.com'],
+      unique: [true, 'Email is exists'],
+      match: emailRegexp,
+    },
+    birthday: {
+      type: Date,
+      required: [true, 'Birthday is required'],
+      dafault: defBirthday,
+    },
+    isAdult: {
+      type: Boolean,
+      dafault: false,
+    },
+    avatarURL: {
+      type: String,
+    },
+    subscribe: {
+      type: Boolean,
+      dafault: false,
+    },
+    token: {
+      type: String,
+      dafault: null,
+    },
+    verificationToken: {
+      type: String,
+      dafault: '',
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+userSchema.post("save", handleMongooseError);
+
+const signUpSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+    birthday: Joi.date().required(),
+});
+
+const signInSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+})
+
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const updateNameSchema = Joi.object({
+    name: Joi.string().required(),
+});
+
+const schemas = {
+  signUpSchema,
+  signInSchema,
+  updateNameSchema,
+  emailSchema,
+};
+
+const User = model("user", userSchema);
+
+export { User, schemas };

6d14ec2bd5dd36a7e79d76b72a5452f62b228ba9
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-30 18:08:24 +0100

    updating logOut

--- src/controllers/auth/logOut.js
+++ src/controllers/auth/logOut.js
@@ -2,8 +2,16 @@ import { User } from '../../models/users.js';
 
 export const logOut = async (req, res) => {
   try {
+    if (!req.user || !req.user._id) {
+      return res.status(400).json({ message: 'Invalid request' });
+    }
+
     const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: '' });
+    const user = await User.findByIdAndUpdate(_id, { token: '' });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
     res.status(204).json({
       message: 'Logout success',
     });

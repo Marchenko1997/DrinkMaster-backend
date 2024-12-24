e77fea9309cdcfcde87885565e177a4be5b18f1e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-12-24 11:44:45 +0200

    updating logs getById

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,6 +5,7 @@ export const isValidId = (req, res, next) => {
   const { id } = req.params;
 
   if (!isValidObjectId(id)) {
+     console.error(`${id} is not a valid MongoDB ObjectId`);
     next(HttpError(404, `${id} is not valid id`));
   }
   next();

eaa049a2d7db74e3a11b46dccfe7477f8608d59d
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-12-24 11:59:08 +0200

    Update getById.js

--- src/controllers/drinks/getById.js
+++ src/controllers/drinks/getById.js
@@ -8,6 +8,7 @@ export const getById = async (req, res) => {
     const result = await Drink.findById(id).populate(
       'ingredients.ingredientId',
     );
+     console.log('Result from DB:', result); 
 
     if (!result) {
       console.error('Drink not found for ID:', id);
@@ -16,7 +17,7 @@ export const getById = async (req, res) => {
      console.log('Drink found:', result);
     res.status(200).json(result);
   } catch (error) {
-      console.error('Error fetching drink:', error.message); 
+      console.error('Error fetching drink:', error.message);
     res.status(error.status || 500).json({ message: error.message });
   }
 };
